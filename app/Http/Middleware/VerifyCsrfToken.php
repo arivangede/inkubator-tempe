d0a0b23bb8a00ee2d5455fc10b7f1a16e6df1fd7
Arivan G. Eka Satria <arivan.gede@gmail.com>
2024-06-14 22:58:51 +0800

    [M]: update

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,10 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        '/post-data',
+        '/data-sensor',
+        '/data-histori',
+        '/control',
+        '/mode',
     ];
 }
